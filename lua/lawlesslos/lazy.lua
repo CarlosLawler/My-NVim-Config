01355de6ad27ae95bcec27ec98073da48ee56535
CarlosLawler <122853311+CarlosLawler@users.noreply.github.com>
2026-03-24 04:31:52 -0400

    added lualine, comment, and auto-pairs
    lualine - upgraded status bar
    
    comment - allows for quick commenting of lines
    
    autopairs - automatically closes things like parenthesis, brackets, etc

--- lua/lawlesslos/lazy.lua
+++ lua/lawlesslos/lazy.lua
@@ -5,14 +5,14 @@
 -- Install lazy.nvim if not already installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 
 -- Add lazy.nvim to runtime path
@@ -20,28 +20,85 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Plugin definitions
 require("lazy").setup({
-  -- Utility library (required by many plugins)
-  { "nvim-lua/plenary.nvim" },
+    -- Utility library (required by many plugins)
+    { "nvim-lua/plenary.nvim" },
 
-  -- Fuzzy finder (files, grep, etc.)
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+    -- Fuzzy finder (files, grep, etc.)
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
 
-  -- Syntax highlighting and parsing
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {}
-  },
+    -- Syntax highlighting and parsing
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {}
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
+
+    {
+        "numToStr/Comment.nvim",
+        event = "VeryLazy",
+        opts = {},{
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            options = {
+                theme = "auto",
+                globalstatus = false,
+                section_separators = "",
+                component_separators = "",
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+                lualine_c = { { "filename", path = 1 } },
+                lualine_x = { "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+        },
+    }
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            options = {
+                theme = "auto",
+                globalstatus = false,
+                section_separators = "",
+                component_separators = "",
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+                lualine_c = { { "filename", path = 1 } },
+                lualine_x = { "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+        },
+    },
 }) 
